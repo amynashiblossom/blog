de6a2f0a0026c351bfbce8521fefb641d88c7331
amynashiblossom <amyhwon.lee@gmail.com>
2026-06-27 22:26:37 +0900

    fix: remove broken plugin import completely

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -2,17 +2,12 @@ import React from "react"
 import { i18n } from "../i18n"
 import { FullSlug, joinSegments, pathToRoot } from "../util/path"
 import { CSSResourceToStyleElement, JSResourceToScriptElement } from "../util/resources"
-import { googleFontHref, googleFontSubsetHref } from "../util/theme"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import { unescapeHTML } from "../util/escape"
 
 export default (() => {
   const Head: QuartzComponent = ({ cfg, fileData, externalResources }: QuartzComponentProps) => {
-    const titleSuffix = cfg.pageTitleSuffix ?? ""
-    const title = (fileData.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title) + titleSuffix
-    const description = fileData.frontmatter?.description ?? fileData.description?.trim() ?? i18n(cfg.locale).propertyDefaults.description
-    const { css, js, additionalHead } = externalResources
-    const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
+    const title = (fileData.frontmatter?.title ?? i18n(cfg.locale).propertyDefaults.title)
+    const { css, js } = externalResources
     const baseDir = fileData.slug === "404" ? "/" : pathToRoot(fileData.slug!)
     const iconPath = joinSegments(baseDir, "static/icon.png")
 
@@ -21,8 +16,6 @@ export default (() => {
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
         <link rel="icon" href={iconPath} />
         {css.map((resource) => CSSResourceToStyleElement(resource, true))}
         {js.filter((resource) => resource.loadTime === "beforeDOMReady").map((res) => JSResourceToScriptElement(res, true))}
