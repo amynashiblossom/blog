a400d7d0cda168872879664f554c3bded9882620
amynashiblossom <120073118+amynashiblossom@users.noreply.github.com>
2026-06-29 10:38:55 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Kairos PM Blog",
+    pageTitle: "My Blog",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
